187677bfe7cfb431c1283941f2416df91d23ee96
Sumit Raj <trapgamingpubg@gmail.com>
2025-01-30 18:33:25 +0530

    some changes

--- script.js
+++ script.js
@@ -11,67 +11,61 @@ const numbers = document.querySelector("#checkbox3");
 const symbols = document.querySelector("#checkbox4");
 
 // immediately invoked function
-(function(){
+(function () {
     passwordLength.textContent = passwordSlider.value;
     passwordMin.textContent = passwordSlider.min;
     passwordMax.textContent = passwordSlider.max;
-    passwordSlider.style.setProperty("--width-value", ((100/(passwordSlider.max - passwordSlider.min)) * (passwordSlider.value-passwordSlider.min)) + "%")
-})()
-
+    passwordSlider.style.setProperty("--width-value", (100 / (passwordSlider.max - passwordSlider.min)) * (passwordSlider.value - passwordSlider.min) + "%");
+})();
 
 // adding functiality of changing length and changing width while sliding the slider
 passwordSlider.addEventListener("input", (e) => {
     passwordLength.textContent = e.currentTarget.value;
-    e.currentTarget.style.setProperty("--width-value", ((100/(e.currentTarget.max - e.currentTarget.min)) * (e.currentTarget.value-e.currentTarget.min)) + "%")
-})  
-
+    e.currentTarget.style.setProperty("--width-value", (100 / (e.currentTarget.max - e.currentTarget.min)) * (e.currentTarget.value - e.currentTarget.min) + "%");
+});
 
 const upperCaseChars = "ABCDEFGHIJKLMNOPQSRTUVWXYZ";
 const lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
 const numbersChars = "0123456789";
 const symbolsChars = "!@#$%&*_";
 
-
 // functions to get a random character from each types of cases
-function getUpperCase(){
-    return upperCaseChars[Math.floor(Math.random() * upperCaseChars.length)]
+function getUpperCase() {
+    return upperCaseChars[Math.floor(Math.random() * upperCaseChars.length)];
 }
-function getLowerCase(){
-    return lowerCaseChars[Math.floor(Math.random() * lowerCaseChars.length)]
+function getLowerCase() {
+    return lowerCaseChars[Math.floor(Math.random() * lowerCaseChars.length)];
 }
-function getNumber(){
-    return numbersChars[Math.floor(Math.random() * numbersChars.length)]
+function getNumber() {
+    return numbersChars[Math.floor(Math.random() * numbersChars.length)];
 }
-function getSymbol(){
-    return symbolsChars[Math.floor(Math.random() * symbolsChars.length)]
+function getSymbol() {
+    return symbolsChars[Math.floor(Math.random() * symbolsChars.length)];
 }
-
-
 
 // generatePassword function
-function generatePassword(isUpperCase, isLowerCase, isNumbers, isSymbols, length){
+function generatePassword(isUpperCase, isLowerCase, isNumbers, isSymbols, length) {
     let password = "";
-    if (!isUpperCase && !isLowerCase && !isNumbers &&!isSymbols){
-        password = ""
-    }else{
-        for (let i = 0; i<length; i++){    
-            if (isUpperCase){
+    if (!isUpperCase && !isLowerCase && !isNumbers && !isSymbols) {
+        password = "";
+    } else {
+        for (let i = 0; i < length; i++) {
+            if (isUpperCase) {
                 password += getUpperCase();
             }
-            if (isLowerCase){
+            if (isLowerCase) {
                 password += getLowerCase();
             }
-            if (isNumbers){
+            if (isNumbers) {
                 password += getNumber();
             }
-            if (isSymbols){
+            if (isSymbols) {
                 password += getSymbol();
             }
         }
     }
     const finalPassword = password.slice(0, length);
     return finalPassword;
-
 
     // second method
 
@@ -91,7 +85,7 @@ function generatePassword(isUpperCase, isLowerCase, isNumbers, isSymbols, length
     //         detailsCaseTrue.forEach((item) => {
     //             const keyItem = Object.keys(item)[0];
     //             if (keyItem === "isUpperCase"){
-    //                 password += getUpperCase() 
+    //                 password += getUpperCase()
     //             }else if (keyItem === "isLowerCase"){
     //                 password += getLowerCase();
     //             }else if (keyItem === "isNumbers"){
@@ -105,40 +99,19 @@ function generatePassword(isUpperCase, isLowerCase, isNumbers, isSymbols, length
     //     const finalPassword = password.slice(0, length)
     //     return finalPassword;
     // }
-
-
 }
 
 // when we click on the generate password button
 passwordGeneratorBtn.addEventListener("click", (e) => {
     const generatedPassword = generatePassword(upperCase.checked, lowerCase.checked, numbers.checked, symbols.checked, +passwordSlider.value);
     password.textContent = generatedPassword;
-})
-
+});
 
 // when we click on passwordCopy button
 passwordCopyBtn.addEventListener("click", (e) => {
     const pass = password.textContent;
-    if (pass){
+    if (pass) {
         // if password exist
         navigator.clipboard.writeText(pass);
     }
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
